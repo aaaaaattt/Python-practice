39b293de11c766f10281fd4e152ab607744f2524
ch <chlgur13@kakao.com>
2023-09-27 22:15:50 +0900

    Modifications are done

--- hw1.py
+++ hw1.py
@@ -5,18 +5,25 @@ f = open("test.txt",'r')
 lines = f.readlines() # 파일의 내용이 line의 배열에 담김
 lines = [line.rstrip('\n') for line in lines] #공백문자 제거
 
+
 def evalPostfix(expr) :
      s = ArrayStack(100)
      for token in expr :
      #    print(token)
         if token in "+-*/%":
-               val2 = s.pop()
-               val1 = s.pop()
-               if (token == '+'): s.push(val1 + val2)
-               elif (token == '-'):s.push(val1 - val2)
-               elif (token == '*'):s.push(val1 * val2)
-               elif (token == '/'):s.push(int(val1 / val2))
-               elif (token == '%'):s.push(val1 % val2)
+               try:
+                    val2 = s.pop()
+                    val1 = s.pop()
+                    if (token == '+'): s.push(val1 + val2)
+                    elif (token == '-'):s.push(val1 - val2)
+                    elif (token == '*'):s.push(val1 * val2)
+                    elif (token == '/'):
+                         if(val2 == 0):
+                              print("divide by 0")
+                              break
+                         s.push(int(val1 / val2))
+                    elif (token == '%'):s.push(val1 % val2)
+               except:print('stack empty')
         elif token == '++' or token == '--':
              val3 = s.pop()
              if(token == '++'): 
@@ -26,7 +33,10 @@ def evalPostfix(expr) :
                   val3 -= 1
                   s.push(val3)
         elif token =='p':
-             print(s.peek())
+                  if(s.isEmpty()):
+                       print('stack empty')
+                       break
+                  print(s.peek())
         elif token =='c':
              s.top = -1
         elif token =='l':
@@ -41,8 +51,8 @@ def evalPostfix(expr) :
           #    print('top 1 증가')
              s.push(int(token))
           #    print('top의 위치:',s.top)
-
+     
      return s.pop()
 
 
-print(evalPostfix(lines))
+evalPostfix(lines)
